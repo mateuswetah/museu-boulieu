4c7da318e6fe882df12ccf2db17e60737456461a
mateuswetah <mateus.m.luna@gmail.com>
2021-03-16 23:40:21 -0300

    Uses blocksy filter for color palettes.

--- functions.php
+++ functions.php
@@ -31,3 +31,35 @@ function museu_boulieu_page_side_layout() {
 <?php
 }
 add_action( 'blocksy:single:content:bottom', 'museu_boulieu_page_side_layout' );
+
+/* Filters possible Blocksy color palletes */
+function museu_boulieu_filter_palette( $args ) {
+	return array(
+		array(
+			'name' => 'Vermelho Boulieu 1',
+			'slug' => 'palette-color-1',
+			'color' => 'var(--paletteColor1, #8b3527)'
+		),
+		array(
+			'name' => 'Vermelho Boulieu 2',
+			'slug' => 'palette-color-2',
+			'color' => 'var(--paletteColor2, #a13121)'
+		),
+		array(
+			'name' => 'Cinza Boulieu 1',
+			'slug' => 'palette-color-3',
+			'color' => 'var(--paletteColor3, #231f20)'
+		),
+		array(
+			'name' => 'Cinza Boulieu 2',
+			'slug' => 'palette-color-4',
+			'color' => 'var(--paletteColor4, #272525)'
+		), 
+		array(
+			'name' => 'Branco Boulieu',
+			'slug' => 'palette-color-5',
+			'color' => 'var(--paletteColor5, #ffffff)'
+		)
+	);
+}
+add_filter( 'blocksy:editor-color-palette', 'museu_boulieu_filter_palette' );
