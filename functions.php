ab7d91c20a88c3ac63214aa2c33e10190e20e2e1
mateuswetah <mateus.m.luna@gmail.com>
2021-03-30 22:21:51 -0300

    Adds local version of ACF for the sidebar elements.

--- functions.php
+++ functions.php
@@ -27,43 +27,6 @@ function museu_boulieu_editor_side_enqueues() {
 }
 add_action( 'enqueue_block_editor_assets', 'museu_boulieu_editor_side_enqueues');
 
-/* Renders the content of the aside part of pages and posts, bellow the title */
-function museu_boulieu_page_side_layout() {
-	global $post;
-?>
-	<div class="museu-boulieu-extra-page-info">
-		<?php 
-			$aside_post_image_1_id = get_post_meta($post->ID, 'imagem_lateral_1');
-
-			if ( $aside_post_image_1_id ): 
-
-				if ( is_array($aside_post_image_1_id) && !empty($aside_post_image_1_id) )
-					$aside_post_image_1_id = $aside_post_image_1_id[0];
-
-				$aside_post_image_1 = wp_get_attachment_image( $aside_post_image_1_id, 'medium', true );
-
-				if ( $aside_post_image_1 ) :
-			?>
-			<figure>
-				<?php 
-					echo $aside_post_image_1;
-
-					$aside_post_image_1_caption = wp_get_attachment_caption( $aside_post_image_1_id );
-					if ( $aside_post_image_1_caption  ) :
-				?>
-				<figcaption>
-					<?php echo $aside_post_image_1_caption; ?>
-				</figcaption>
-				<?php endif; ?>
-			</figure>
-			<?php endif; ?>
-		
-		<?php endif; ?>
-	</div>
-<?php
-}
-add_action( 'blocksy:single:content:bottom', 'museu_boulieu_page_side_layout' );
-
 /* Filters possible Blocksy color palletes */
 function museu_boulieu_filter_palette( $args ) {
 	return array(
@@ -101,11 +64,11 @@ function museu_boulieu_filter_palette( $args ) {
 }
 add_filter( 'blocksy:editor-color-palette', 'museu_boulieu_filter_palette' );
 
+// Registers CF for displaying images on the sidebar
+require get_stylesheet_directory() . '/inc/block-styles.php';
+
 // Registers Block styles
 require get_stylesheet_directory() . '/inc/block-styles.php';
 
 // Registers Block patterns
 require get_stylesheet_directory() . '/inc/block-patterns.php';
-
-// Registers Carousel Shortcode
-require get_stylesheet_directory() . '/inc/carousel-shortcode.php';
---
+++ inc/sidebar.php
@@ -0,0 +1,121 @@
+<?php
+
+/* Registers the custom post field for sidebar items, using ACF plugin */
+if ( function_exists('acf_add_local_field_group') ) {
+
+    acf_add_local_field_group(array(
+        'key' => 'group_6052830124756',
+        'title' => 'Imagens laterais',
+        'fields' => array(
+            array(
+                'key' => 'field_605283283a9ab',
+                'label' => 'Imagem Lateral 1',
+                'name' => 'imagem_lateral_1',
+                'type' => 'image',
+                'instructions' => 'Insira aqui alguma imagem que você deseja usar para complementar o seu post ou pagina, na barra lateral abaixo do título',
+                'required' => 0,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'return_format' => 'id',
+                'preview_size' => 'medium',
+                'library' => 'all',
+                'min_width' => '',
+                'min_height' => '',
+                'min_size' => '',
+                'max_width' => '',
+                'max_height' => '',
+                'max_size' => '',
+                'mime_types' => '',
+            ),
+            array(
+                'key' => 'field_605283b83a9ac',
+                'label' => 'Imagem Lateral 2',
+                'name' => 'imagem_lateral_2',
+                'type' => 'image',
+                'instructions' => 'Insira aqui alguma outra imagem que você deseja usar para complementar o seu post ou pagina, na barra lateral abaixo do título',
+                'required' => 0,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'return_format' => 'id',
+                'preview_size' => 'medium',
+                'library' => 'all',
+                'min_width' => '',
+                'min_height' => '',
+                'min_size' => '',
+                'max_width' => '',
+                'max_height' => '',
+                'max_size' => '',
+                'mime_types' => '',
+            ),
+        ),
+        'location' => array(
+            array(
+                array(
+                    'param' => 'post_type',
+                    'operator' => '==',
+                    'value' => 'post',
+                ),
+            ),
+            array(
+                array(
+                    'param' => 'page_template',
+                    'operator' => '==',
+                    'value' => 'default',
+                ),
+            ),
+        ),
+        'menu_order' => 0,
+        'position' => 'side',
+        'style' => 'default',
+        'label_placement' => 'top',
+        'instruction_placement' => 'label',
+        'hide_on_screen' => '',
+        'active' => true,
+        'description' => '',
+    ));
+}
+
+/* Renders the content of the aside part of pages and posts, bellow the title */
+function museu_boulieu_page_side_layout() {
+	global $post;
+?>
+	<div class="museu-boulieu-extra-page-info">
+		<?php 
+			$aside_post_image_1_id = get_post_meta($post->ID, 'imagem_lateral_1');
+
+			if ( $aside_post_image_1_id ): 
+
+				if ( is_array($aside_post_image_1_id) && !empty($aside_post_image_1_id) )
+					$aside_post_image_1_id = $aside_post_image_1_id[0];
+
+				$aside_post_image_1 = wp_get_attachment_image( $aside_post_image_1_id, 'medium', true );
+
+				if ( $aside_post_image_1 ) :
+			?>
+			<figure>
+				<?php 
+					echo $aside_post_image_1;
+
+					$aside_post_image_1_caption = wp_get_attachment_caption( $aside_post_image_1_id );
+					if ( $aside_post_image_1_caption  ) :
+				?>
+				<figcaption>
+					<?php echo $aside_post_image_1_caption; ?>
+				</figcaption>
+				<?php endif; ?>
+			</figure>
+			<?php endif; ?>
+		
+		<?php endif; ?>
+	</div>
+<?php
+}
+add_action( 'blocksy:single:content:bottom', 'museu_boulieu_page_side_layout' );
